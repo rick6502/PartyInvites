2e9d9250dc1d28d705149e021027aaeeb2ff3c89
RIck Fung <rick2032@hotmail.com>
2020-09-21 23:09:52 -0700

    Updated HomeController w/ new action & view
    Added a new action and a strongly typed view to the HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,5 +11,10 @@ namespace PartyInvites.Controllers
             ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
             return View("MyView");
         }
+
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
     }
 }
